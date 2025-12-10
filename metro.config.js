e3ccb495a66f3b6869f989e505922bae74b5b0a0
anvitha1633 <anvithashet@gmail.com>
2025-12-10 10:15:18 +0530

    server folder change

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,3 @@
-// metro.config.js
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require("metro-config");
 
-const defaultConfig = getDefaultConfig(__dirname);
-
-const config = {
-    transformer: {
-        assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-    },
-
-    resolver: {
-        assetExts: [
-            ...defaultConfig.resolver.assetExts,
-            'png',
-            'jpg',
-            'jpeg',
-            'webp'
-        ],
-    },
-};
-
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = getDefaultConfig(__dirname);
